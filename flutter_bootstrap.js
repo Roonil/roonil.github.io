f66f8b92e087bce9b9567204098beadc9e77b785
Roonil <anandverma458@gmail.com>
2026-01-14 22:01:54 +0530

    Updated visibility description, now handles optional arguments

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0e9461ea4aef6675ed8ed87298985e308540af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "320983874" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3759016161" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
